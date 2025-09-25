91b9b01870aafadeb9ff90e16730aec9b3b753a7
RJVB09 <49564898+RJVB09@users.noreply.github.com>
2025-09-25 22:03:13 +0200

    Word loading and random picking

--- hanzile/filter.py
+++ hanzile/filter.py
@@ -3,7 +3,8 @@ import numpy as np
 import random as r
 
 dataset = pd.read_csv('hanzile/hsk3words.csv')
-words = list(dataset['word_simplified'])
+mask = dataset['word_simplified'].str.len() == 2
+words = list(dataset[mask]['word_simplified'])
 
 mask = (
     (dataset['word_simplified'].str.len() == 1)
@@ -19,3 +20,18 @@ df = pd.DataFrame(characters, columns=['character'])
 
 df.to_csv('hanzile/characters.csv')
 df.to_json('hanzile/characters.json')
+
+
+# Apply the mask
+mask = dataset['word_simplified'].str.len() == 2
+words = dataset[mask]
+
+# Select only the columns you want and rename them
+words = words[['word_simplified', 'pinyin', 'cc_cedict_english_definition']].rename(
+    columns={
+        'word_simplified': 'word',
+        'cc_cedict_english_definition': 'definition'
+    }
+)
+
+words.to_csv('hanzile/words.csv')
